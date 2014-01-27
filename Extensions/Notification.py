bdba2de71be2948521061499d74fc076448f771b
Amoatey Harrison <fortharris@gmail.com>
2014-01-27 09:34:55 +0000

    Animated Notification Bar
    Usually when there is a notification and an old one with the same
    message already exists, you are not able to tell if there is a new
    notification. With the new animation, that is easy to tell.

--- Extensions/Notification.py
+++ Extensions/Notification.py
@@ -1,4 +1,4 @@
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 
 
 class Notification(QtGui.QLabel):
@@ -8,6 +8,12 @@ class Notification(QtGui.QLabel):
 
         self.setMinimumHeight(25)
         self.setMargin(5)
+        
+        self.easingCurve = QtCore.QEasingCurve.OutCubic
+
+        self.showAnimation = QtCore.QPropertyAnimation(self, 'maximumWidth')
+        self.showAnimation.setDuration(200)
+        self.showAnimation.setEasingCurve(self.easingCurve)
 
         self.setStyleSheet("""background: #1A1A1A;
                                 color: white;
@@ -20,6 +26,14 @@ class Notification(QtGui.QLabel):
         self.hide()
 
     def showMessage(self, mess):
+        self.hide()
         self.setText(mess)
-        self.show()
         self.adjustSize()
+        
+        max_width = self.geometry().width()
+        self.showAnimation.setEndValue(max_width + 10)
+        self.setMaximumWidth(0)
+        self.show()
+        self.showAnimation.start()
+        
+        
